a6e235e352c5105976f1e1c2fe9aab9e9786bb77
SILMARA DE PAULA SANTOS <166147948+silmara13@users.noreply.github.com>
2025-02-25 08:58:43 -0300

    Add files via upload

---
+++ Aula 2402/Perfil.java
@@ -0,0 +1,11 @@
+public class Perfil{
+
+    public static void main(String [] args) {
+    System.out.println( "Nome: Silmara Santos");
+    System.out.println( "Idade:30 anos");
+    System.out.println( "Interesse:Treino, filmes e comidinhas boas");
+    
+    }
+    
+    }
+    
